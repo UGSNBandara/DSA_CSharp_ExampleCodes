95914ef332145326ebc28579d5ad69c0cd90a8f1
UGSNBandara <sulithanb119@gmail.com>
2025-02-14 10:24:38 +0530

    Add project files.

---
+++ DynamicArray.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.ComponentModel;
+
+
+namespace DSA_CSharp
+{
+    public class DynamicArray
+    {
+        private int[] array;
+        private int count;
+        private int size;
+
+        public DynamicArray()
+        {
+            array = new int[1];
+            count = 0;
+            size = 1;
+        }
+
+        //Print the Array
+        public void PrintArray()
+        {
+            for (int i =0; i< count; i++)
+            {
+                Console.Write(array[i]);
+                Console.Write(" ");
+            }
+
+            Console.WriteLine();
+        }
+
+        //Add Element to Array
+        public void AddElement(int newValue)
+        {
+            if (count == size)
+            {
+                GrowSize();
+            }
+            array[count] = newValue;
+            count++;
+        }
+
+        //Add an Element at Given Index
+        public void AddElementAt(int ind, int newValue)
+        {
+            if (ind > count || ind < 0)
+            {
+                Console.WriteLine("Index is OutofBound So add to the End of the Array");
+                AddElement(newValue);
+                return;
+            }
+
+            if (count == size)
+            {
+                GrowSize();
+            }
+
+            for (int i = count - 1; i >= ind; i--)
+            {
+                array[i + 1] = array[i];
+            }
+
+            array[ind] = newValue;
+            count++;
+        }
+
+        //Grow the size of Array By *2
+        private void GrowSize()
+        {
+            int[] temp = null;
+            if (count == size)
+            {
+                temp = new int[size * 2];
+                for (int i = 0; i < size; i++)
+                {
+                    temp[i] = array[i];
+                }
+                array = temp;
+                size = size * 2;
+            }
+        }
+
+        // To Remove extra spaces ( Srink Array)
+        private void SrinkArray()
+        {
+            int[] temp = null;
+            if (count > 0)
+            {
+                temp = new int[count];
+                for (int i = 0; i < count; i++)
+                {
+                    temp[i] = array[i];
+                }
+
+                array = temp;
+                size = count;
+            }
+        }
+
+        //Remove Element at the End
+        public void RemoveAtEnd()
+        {
+            if (count > 0)
+            {
+                array[count - 1] = 0;
+                count--;
+            }
+        }
+
+        public void RemoveAt(int index) 
+        {
+            if (count > 0)
+            {
+                if( 0<=index || index < count)
+                {
+                    for(int i = index; i< count-1; i++)
+                    {
+                        array[i] = array[i + 1];
+                    }
+                }
+
+                array[count - 1] = 0;
+                count--;
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DSA_CSharp
+{
+
+
+    class Program
+    {
+        static void Main()
+        {
+            //since the TestDynamicArray is not a staticMethod, we need to create an object and access instance method inside the Static methods
+            Program p = new Program();
+            p.TestDynamicArray();
+        }
+
+
+        // Test Dynamic Array Class
+        private void TestDynamicArray()
+        {
+            DynamicArray darray = new DynamicArray();
+            darray.AddElement(10);
+            darray.AddElement(20);
+            darray.AddElement(30);
+            darray.AddElement(40);
+
+            darray.PrintArray();
+
+            darray.AddElementAt(1, 15);
+            darray.AddElementAt(3, 25);
+
+            darray.PrintArray();
+
+            darray.RemoveAt(0);
+            darray.RemoveAt(2);
+
+            darray.PrintArray();
+        }
+    }
+}
